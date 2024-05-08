fada99d100440a7ac59e3b577aa249a29b59c3ab
Inna-decsgr <anjgkwl27@gmail.com>
2024-05-09 05:59:30 +0900

    Modify inappropriate component name

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -10,7 +10,7 @@ interface IPaginationProps {
   contentsPerPage: number;
 }
 
-export default function FilterablePosts({
+export default function Pagination({
   currentPage,
   totalContents,
   paginate,
